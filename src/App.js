8ee0ab6754f2954f2b293ad78d22b65059876be3
BenTWouo <https://github.com/BenTWouo>
2024-05-01 15:07:37 +0800

    up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           "5B0G0021 2024.5.1"
+           <code>"5B0G0021 2024.5.1"</code>
         </p>
         <a
           className="App-link"
