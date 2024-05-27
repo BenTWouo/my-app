e8424793126f368d8398d29747fb6863ba2d1c2a
BenTWouo <bentwouo@gmail.com>
2024-05-27 09:49:25 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import MyRGBPanel from "./components/MyRGBPanel";
-import MySlider from "./components/MySlider";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
     <div className="App">
-      <h1>5B0G0021 林克</h1>
       <h1>MySlider</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
     </div>
   );
 }
